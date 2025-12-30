d71760119485512fa92ffb631b1a68f4d96c19fc
Nek1t_ <144207428+Nek1tt@users.noreply.github.com>
2025-12-30 15:29:28 +0700

    Delete frontend/build directory

--- frontend/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_musicproject_FILE C:/Users/Lenovo/upprpo/YourMusic/frontend/build/musicproject.exe)
-set(__QT_DEPLOY_TARGET_musicproject_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_musicproject_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Users/Lenovo/upprpo/qtwebsockets/build/bin/Qt6WebSockets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
--- frontend/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_musicproject_FILE C:/Users/Lenovo/upprpo/musicproject/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/musicproject.exe)
-set(__QT_DEPLOY_TARGET_musicproject_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_musicproject_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
